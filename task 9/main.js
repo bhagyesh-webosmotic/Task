d245c1c9bd28341a117110855e8bc33b7176ce1e
bhagyesh-webosmotic <bhagyesh.webosmotic@gmail.com>
2021-07-07 14:27:26 +0530

    removed attrib.js

--- task 9/main.js
+++ task 9/main.js
@@ -64,3 +64,40 @@ function removeTempRowLoop(targets) {
 		removeTempRowLoop(targets);
 	}
 }
+
+function attrib(input, element) {
+	if (!element.value) {
+		// console.log("inside dom");
+		switch (element.type) {
+			case "range":
+				input.setAttribute("min", 0);
+				input.setAttribute("max", 10);
+				break;
+		}
+		input.setAttribute("type", element.type);
+		input.setAttribute("name", element.id);
+		input.setAttribute("value", element.id);
+		input.setAttribute("id", element.id);
+		if (element.type == "color") {
+			input.setAttribute("class", "colorPicker");
+		} else {
+			input.setAttribute("class", "formFields");
+		}
+	} else {
+		// console.log("inside saved data");
+		switch (element.type) {
+			case "range":
+				input.setAttribute("min", 0);
+				input.setAttribute("max", 10);
+				break;
+		}
+		input.setAttribute("type", element.type);
+		input.setAttribute("value", element.value);
+		input.setAttribute("id", element.id);
+		if (element.type == "color") {
+			input.setAttribute("class", "colorPicker");
+		} else {
+			input.setAttribute("class", "formFields");
+		}
+	}
+}
